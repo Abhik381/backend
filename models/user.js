9056e2878bc49f2a75bcbede9629955e07f17e1a
Abhik381 <168610652+Abhik381@users.noreply.github.com>
2024-05-01 22:07:09 +0530

    Connect database

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("");
+mongoose.connect("mongodb+srv://abhik:abhik2005@backend.xw7qlrx.mongodb.net/mini_project_1");
 
 const userSchema = mongoose.Schema({
     username: String,
