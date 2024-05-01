c131d1da1eec5e7cf03252c4f8a2b57f8ba91e08
Abhik381 <168610652+Abhik381@users.noreply.github.com>
2024-05-01 17:58:40 +0000

    register,  login and logout routs create

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const userModel = require("./models/user");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -17,8 +18,48 @@ app.get("/login",function(req,res){
   res.render("login")
 })
 
-app.post("/register", function(req,res){
+app.get("/profile", function(req,res){
+  res.render("profile");
+})
 
+app.post("/register", async function(req,res){
+  let {username,email,number,password} = req.body;
+  let user = await userModel.findOne({email});
+  if(user) res.status(300).send("User already registered.");
+  
+  bcrypt.genSalt(10, function(err,salt){
+    bcrypt.hash(password,salt, async function(err,hash){
+     let user = await userModel.create({
+        username,
+        email,
+        number,
+        password: hash
+      })
+
+     let token = jwt.sign({email: email, userId: user._id},"abhikmondal");
+     res.cookie("token", token);
+     res.redirect("/profile")
+    })
+  })
+})
+
+app.post("/login", async function(req,res){
+  let {email,password} = req.body;
+  let user = await userModel.findOne({email});
+  if(!user) res.status(500).send("Something went wrong.");
+
+  bcrypt.compare(password,user.password, function(err,result){
+    if(result){
+      let token = jwt.sign({email: email, userId: user._id}, "abhikmondal");
+      res.cookie("token", token);
+      res.redirect("/profile");
+    }
+  })
+})
+
+app.post("/logout", function(req,res){
+  res.cookie("token", "");
+  res.redirect("/login");
 })
 
 app.listen(3000, function(req,res){
